cb4b4c60ebd28892ad96990ec86f667611963316
Andre <dtandre331@gmail.com>
2023-05-13 00:30:01 -0400

    Added GetStarted Page
    - implemented a basic get started page with four sub pages that will allow the user to see what the app is all about
    - the fourth page will be a login page

--- frontend/screens/FootprintCalc.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Picker, Button } from 'react-native';
-
-export default function Calculator() {
-    return (
-        <Text>Calculator</Text>
-    );
-}
--- frontend/screens/GetStarted.js
+++ frontend/screens/GetStarted.js
@@ -1,28 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Picker, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function Beginning() {
+    const [activePage, setActivePage] = useState(0);
+    const subpages = [
+        { title: "Page 1", content: "Page 1 content" },
+        { title: "Page 2", content: "Page 2 content" },
+        { title: "Page 3", content: "Page 3 content" },
+        { title: "Page 4", content: "Page 4 content" },
+    ];
+
+    const handleNextPage = function () {
+        setActivePage(activePage + 1);
+    };
+
+    const handlePrevPage = function () {
+        setActivePage(activePage - 1);
+    };
+
     return (
+        // instead of styles.titles and styles.content, use Calendar.js for second page, Tips.js for third page
+        // and LoginPage.js for the fourth page
         <View style={styles.container}>
-            <Text style={styles.TrackYourFootprint}>Track Your Carbon Footprint</Text>
-            <Text style={styles.getStarted}>Get Started</Text>
-            <StatusBar style="auto" />
+            <View style={styles.subPage}> 
+                <Text style={styles.title}>{subpages[activePage].title}</Text>
+                <Text style={styles.content}>{subpages[activePage].content}</Text>
+            </View>
+        <View style={styles.dots}>
+            <View style={[styles.dot, activePage === 0 && styles.activeDot]} />
+            <View style={[styles.dot, activePage === 1 && styles.activeDot]} />
+            <View style={[styles.dot, activePage === 2 && styles.activeDot]} />
+            <View style={[styles.dot, activePage === 3 && styles.activeDot]} />
+        </View>
+            <View style={styles.footer}>
+                {activePage > 0 && (
+                    <TouchableOpacity style={styles.button} onPress={handlePrevPage}>
+                        <Text style={styles.buttonText}>Previous</Text>
+                    </TouchableOpacity>
+                )}
+                {activePage < subpages.length - 1 ? (
+                    <TouchableOpacity style={styles.button} onPress={handleNextPage}>
+                        <Text style={styles.buttonText}>Next</Text>
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Get Started</Text>
+                    </TouchableOpacity>
+                )}
+            </View>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         marginTop: 50,
         alignItems: 'center',
     },
-    TrackYourFootprint: {
-        color: 'rgb(105, 0, 255)',
+    
+    subPage: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 30,
     },
-    getStarted: {
-        color: 'red',
-        fontSize: 20,
-        fontFamily: 'monospace',
-    }
+    
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    
+    content: {
+        fontSize: 18,
+        textAlign: 'center',
+    },
+    
+    dots: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 20,
+    },
+    
+    dot: {
+        width: 8,
+        height: 8,
+        borderRadius: 4,
+        marginHorizontal: 5,
+        backgroundColor: '#c4c4c4',
+    },
+    
+    activeDot: {
+        backgroundColor: '#333333',
+    },
+    
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingBottom: 30,
+        width: '100%',
+    },
+    
+    button: {
+        backgroundColor: '#333333',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 10,
+    },
+    
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
 });
