642dbe3a7645565704f2060b64fcb08d565d968b
Andre <dtandre331@gmail.com>
2023-05-13 01:53:43 -0400

    installed firebase for login

--- frontend/screens/Calendar.js
+++ frontend/screens/Calendar.js
@@ -34,10 +34,10 @@ const styles = StyleSheet.create({
         backgroundColor: 'blue',
         borderWidth: 1,
         borderColor: 'black',
-      },
+    },
     calendarImage: {
         width: 400,
         height: 300
         
-    }
+    },
 });
