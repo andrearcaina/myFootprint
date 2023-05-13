88cb9c3a35c13081324ac9bdce0f6e3b41efecf7
Tristanv0 <cheng.tristan@hotmail.com>
2023-05-13 01:22:08 -0400

    added image to calendar page

--- frontend/screens/Calendar.js
+++ frontend/screens/Calendar.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Picker, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Picker, Button, Image, } from 'react-native';
 
 export default function SecondPage() {
     return (
@@ -8,6 +8,7 @@ export default function SecondPage() {
             <Text style={styles.TrackYourFootprint}>Track Your Carbon Footprint</Text>
             <Text style={styles.getWeeklyMonthly}>Get Weekly and Monthly Progress! </Text>
             <View style={styles.box}></View>
+            <Image source={require('./images/Calendar-Clipart.png')} style={[styles.calendarImage, {marginRight: 30}]} />
             <StatusBar style="auto" />
         </View>
     );
@@ -20,10 +21,12 @@ const styles = StyleSheet.create({
     },
     TrackYourFootprint: {
         color: 'rgb(105, 0, 255)',
+        fontFamily: 'monospace',
     },
     getWeeklyMonthly: {
         color: 'red',
         fontSize: 20,
+        fontFamily: 'monospace',
     },
     box: {
         width: 100,
@@ -32,4 +35,9 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'black',
       },
+    calendarImage: {
+        width: 400,
+        height: 300
+        
+    }
 });
