4881d9272f873b385712cdbe49a88241f8112859
Andre <dtandre331@gmail.com>
2023-05-12 23:50:34 -0400

    started calculator screen

---
+++ frontend/screens/FootprintCalc.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Picker, Button } from 'react-native';
+
+export default function Calculator() {
+    return (
+        <Text>Hello World!</Text>
+    );
+}
